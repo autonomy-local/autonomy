eefb33f46a561708c3bdaa232f563038db0d7d42
yamashita-kenngo <yamashitakenngo@gmail.com>
2022-12-29 23:07:07 +0900

    fix: css class and test

--- home/tests/root-page.spec.ts
+++ home/tests/root-page.spec.ts
@@ -8,15 +8,15 @@ test('root page health check', async ({page}) => {
   const span = page.locator('.toolbar-title');
   await expect(span).toHaveText('Autonomy');
 
-  const el_home = page.locator('text=Home');
+  const el_home = page.locator('.home');
   await expect(el_home).toHaveAttribute('href', '/home');
 
-  const el_about = page.locator('text=About');
+  const el_about = page.locator('.about');
   await expect(el_about).toHaveAttribute('href', '/about');
 
-  const el_product = page.locator('text=Product');
+  const el_product = page.locator('.product');
   await expect(el_product).toHaveAttribute('href', '/product');
 
-  const el_contribute = page.locator('text=Contribute');
+  const el_contribute = page.locator('.contribute');
   await expect(el_product).toHaveAttribute('href', '/contribute');
 })
