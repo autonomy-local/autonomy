4243d6856ca1dc47a2c1188d71c472d49ee582b0
yamashita-kenngo <yamashitakenngo@gmail.com>
2022-12-29 21:36:06 +0900

    feat: add routing pages

---
+++ home/src/app/contribute-page/contribute-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-contribute-page',
+  templateUrl: './contribute-page.component.html',
+  styleUrls: ['./contribute-page.component.css'],
+})
+export class ContributePageComponent {}
